17061fe674b2c1f22ad1d7ad92fe7c3657e7cec1
Hari Krishna <36559264+HariKrishna61510@users.noreply.github.com>
2021-07-18 17:53:16 +0530

    Update Sqrt with binary_search.cpp

--- LeetCode/Sqrt with binary_search.cpp
+++ LeetCode/Sqrt with binary_search.cpp
@@ -1,3 +1,15 @@
+/*
+Question:
+
+Given a non-negative integer x, compute and return the square root of x.
+
+Since the return type is an integer, the decimal digits are truncated, and only the integer part of the result is returned.
+
+Note: You are not allowed to use any built-in exponent function or operator, such as pow(x, 0.5) or x ** 0.5.
+
+*/
+
+
 class Solution {
     int squareRoot(int left, int right, int target) {
         int result = 0;
