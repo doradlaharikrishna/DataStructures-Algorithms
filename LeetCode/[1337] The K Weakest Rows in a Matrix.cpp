151863fab73163ee747246fbec188284c6cec18f
Hari Krishna <36559264+HariKrishna61510@users.noreply.github.com>
2021-07-18 21:29:18 +0530

    Create [1337] The K Weakest Rows in a Matrix.cpp

---
+++ LeetCode/[1337] The K Weakest Rows in a Matrix.cpp
@@ -0,0 +1,89 @@
+/**
+Qustion:
+
+You are given an m x n binary matrix mat of 1's (representing soldiers) and 0's (representing civilians). The soldiers are positioned in front of the civilians. That is, all the 1's will appear to the left of all the 0's in each row.
+
+A row i is weaker than a row j if one of the following is true:
+
+The number of soldiers in row i is less than the number of soldiers in row j.
+Both rows have the same number of soldiers and i < j.
+Return the indices of the k weakest rows in the matrix ordered from weakest to strongest.
+
+ 
+
+Example 1:
+Input: mat = 
+[[1,1,0,0,0],
+ [1,1,1,1,0],
+ [1,0,0,0,0],
+ [1,1,0,0,0],
+ [1,1,1,1,1]], 
+k = 3
+Output: [2,0,3]
+Explanation: 
+The number of soldiers in each row is: 
+- Row 0: 2 
+- Row 1: 4 
+- Row 2: 1 
+- Row 3: 2 
+- Row 4: 5 
+The rows ordered from weakest to strongest are [2,0,3,1,4].
+
+
+Example 2:
+Input: mat = 
+[[1,0,0,0],
+ [1,1,1,1],
+ [1,0,0,0],
+ [1,0,0,0]], 
+k = 2
+Output: [0,2]
+Explanation: 
+The number of soldiers in each row is: 
+- Row 0: 1 
+- Row 1: 4 
+- Row 2: 1 
+- Row 3: 1 
+The rows ordered from weakest to strongest are [0,2,3,1].
+ 
+
+Constraints:
+m == mat.length
+n == mat[i].length
+2 <= n, m <= 100
+1 <= k <= m
+matrix[i][j] is either 0 or 1.
+*/
+
+class Solution {
+    
+    int search1(vector<int> vec) {
+        int left = 0, right = vec.size()-1;
+        int index = -1;
+        while(left <= right){
+            int mid = left + (right - left) / 2;
+            if(vec[mid] == 1) {
+                index = mid;
+                left = mid + 1;
+            } else{
+                right = mid - 1;
+            }
+        }
+        return index;
+    }
+    
+public:
+    vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
+        vector<pair<int, int> > vec;
+        for (int i=0;i<mat.size();i++){
+            int noOfSoldiers = search1(mat[i]) + 1;
+            vec.push_back({noOfSoldiers, i});
+        }
+        sort(vec.begin(), vec.end());
+        vector<int> res;
+        for(int i=0;i<k;i++){
+            res.push_back(vec[i].second);
+        }
+        return res;
+    }
+};
