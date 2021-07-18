d9c166ddffe033dad931af4c72c2872606f62c04
Hari Krishna <36559264+HariKrishna61510@users.noreply.github.com>
2021-07-18 17:54:56 +0530

    Update binary_search.cpp

--- LeetCode/binary_search.cpp
+++ LeetCode/binary_search.cpp
@@ -1,4 +1,4 @@
-/*
+/**
 Question:
 
 Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
