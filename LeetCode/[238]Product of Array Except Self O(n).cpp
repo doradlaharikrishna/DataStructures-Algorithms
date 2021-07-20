281f83707adfca5e8b426114c7e611b7f13a0cbd
Hari Krishna <36559264+HariKrishna61510@users.noreply.github.com>
2021-07-21 00:07:31 +0530

    Create [238]Product of Array Except Self O(n).cpp

---
+++ LeetCode/[238]Product of Array Except Self O(n).cpp
@@ -0,0 +1,46 @@
+/**
+Question:
+
+Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+
+You must write an algorithm that runs in O(n) time and without using the division operation.
+
+ 
+
+Example 1:
+Input: nums = [1,2,3,4]
+Output: [24,12,8,6]
+
+Example 2:
+Input: nums = [-1,1,0,-3,3]
+Output: [0,0,9,0,0]
+ 
+
+Constraints:
+2 <= nums.length <= 105
+-30 <= nums[i] <= 30
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+*/
+
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        vector<int> resultOfProductExceptSelf(nums.size(), 1);
+        int productSoFar = 1;
+        for(int i = 0; i < nums.size(); i++) {
+            resultOfProductExceptSelf[i] = productSoFar;
+            productSoFar *= nums[i];
+        }
+        
+        productSoFar = 1;
+        
+        for(int i = nums.size() - 1; i >= 0; i--) {
+            resultOfProductExceptSelf[i] *= productSoFar;
+            productSoFar *= nums[i];
+        }
+        
+        return resultOfProductExceptSelf;
+    }
+};
